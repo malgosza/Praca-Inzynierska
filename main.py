bf7618b06e9b5d63744872b825c5de20ba3c5392
Gosia <malgosza.niewiadomska@gmail.com>
2018-10-21 13:07:03 +0200

    Rysowanie obręczy wokół najbardziej niebieskiego punktu

--- main.py
+++ main.py
@@ -2,32 +2,34 @@ import cv2
 import numpy as np
 
 cap=cv2.VideoCapture(0)
+punkty=[]
+
 while(True):
     ret, frame=cap.read()
     b,g,r=cv2.split(frame)
-    i, j = np.unravel_index(b.argmax(), b.shape)
-    # maxIndeks=np.argmax(b)
-    print(str(i)+" "+str(j))
+    maxValue=b.argmax()
+
+    if maxValue>85000:
+        i, j = np.unravel_index(b.argmax(), b.shape)
+        print(str(j)+" "+str(i))
+        cv2.circle(frame,(j,i),15,(0,0,255),2)
+        punkty.append((j,i))
+
     cv2.imshow("RBG", frame)
+    #wrzucNaSiec() -> wchodza punkty wychodziliterka
+    #pisanie w ramce np w rogu jaka litera
+
+    cv2.putText(img=frame,text="OpenCV",
+                org=(200,200),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=2,
+                color=(0,0,0),
+                thickness=,
+                bottomLeftOrigin=False)
     if cv2.waitKey(1) & 0xFF == ord('z'):
         break
 cap.release()
 cv2.destroyAllWindows()
 
-# cap = cv2.VideoCapture(cv2.CAP_INTELPERC_IMAGE)
+print(punkty)
 
-# while(True):
-#     # Capture frame-by-frame
-#     # ret,frame=cap.retrieve(cv2.CAP_OPENNI_DEPTH_MAP)
-#     ret, frame = cap.read()
-#     b, g, r = cv2.split(cap)
-#     print(r)
-#
-#     # Display the resulting frame
-#     cv2.imshow('Finger Detection',frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# # When everything done, release the capture
-# cap.release()
-# cv2.destroyAllWindows()
