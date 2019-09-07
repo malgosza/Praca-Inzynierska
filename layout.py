cbe534e9ff4468ba6e9812ab263c0140e2972315
Gosia <malgosza.niewiadomska@gmail.com>
2019-09-07 22:07:48 +0200

    Adding POMOC Message Box

--- layout.py
+++ layout.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 root=Tk()
 root.geometry('680x450')
@@ -6,6 +7,10 @@ root.title("Małgorzata Niewiadomska Inżynieria Biomedyczna")
 
 def printName():
     print("Hello World!")
+
+def printPOMOC():
+    messagebox.showinfo("POMOC", "POMOC")
+
 
 buttonSTART = Button(root, text='ROZPOCZNIJ', padx=10, pady=10, command=printName)
 buttonSTART.pack(padx=180, pady=200)
@@ -15,7 +20,7 @@ buttonSTART.config(font=('helvetica', 25))
 label=Label(text="Bezdotykowy intrefejs do rozpoznawania znaków \nz wykorzystaniem kamery RGB-D i sztucznych sieci neuronowych",
             font=("arial", 15,'bold')).place(x=15,y=40)
 
-buttonPOMOC=Button(root,text='POMOC',padx=10,pady=10,bg='yellow',fg='black',command=printName).place(x=300,y=300)
+buttonPOMOC=Button(root,text='POMOC',padx=10,pady=10,bg='yellow',fg='black',command=printPOMOC).place(x=300,y=300)
 
 root.resizable(0, 0)
 root.mainloop()
