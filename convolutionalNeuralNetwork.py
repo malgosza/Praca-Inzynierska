76d20eca0093af5633cf16e6a6ba2f37773bba2e
Gosia <malgosza.niewiadomska@gmail.com>
2020-01-02 20:10:13 +0100

    Naprawiam #1

--- convolutionalNeuralNetwork.py
+++ convolutionalNeuralNetwork.py
@@ -7,81 +7,72 @@ import tensorflow as tf
 import cv2
 import os
 
-# punkty=[
-#     (153,161),
-#     (170,138),
-#     (187,115),
-#     (204,115),
-#     (221,115),
-#     (238,115),
-#     (255,115),
-#     (255,138),
-#     (272,161),
-#     (272,184),
-#     (289,184),
-#     (272,207),
-#     (289,207),
-#     (272,230),
-#     (289,230),
-#     (272,253),
-#     (289,253),
-#     (272,276),
-#     (272,299),
-#     (255,299),
-#     (238,322),
-#     (221,322),
-#     (204,345),
-#     (187,368),
-#     (170,368),
-#     (153,391),
-#     (136,391),
-#     (119,414),
-#     (136,414),
-#     (153,414),
-#     (170,414),
-#     (187,414),
-#     (204,414),
-#     (221,414),
-#     (238,414),
-#     (255,414),
-#     (272,414),
-#     (289,414),
-#     (306,414),
-#     (323,414),
-#     (119,437),
-#     (136,437),
-#     (153,437),
-#     (170,437),
-#     (187,437),
-#     (204,437),
-#     (221,437),
-#     (238,437),
-#     (255,437),
-#     (272,437),
-#     (289,437),
-#     (306,437),
-#     (323,437)
-#     ]
+punkty=[
+    (153,161),
+    (170,138),
+    (187,115),
+    (204,115),
+    (221,115),
+    (238,115),
+    (255,115),
+    (255,138),
+    (272,161),
+    (272,184),
+    (289,184),
+    (272,207),
+    (289,207),
+    (272,230),
+    (289,230),
+    (272,253),
+    (289,253),
+    (272,276),
+    (272,299),
+    (255,299),
+    (238,322),
+    (221,322),
+    (204,345),
+    (187,368),
+    (170,368),
+    (153,391),
+    (136,391),
+    (119,414),
+    (136,414),
+    (153,414),
+    (170,414),
+    (187,414),
+    (204,414),
+    (221,414),
+    (238,414),
+    (255,414),
+    (272,414),
+    (289,414),
+    (306,414),
+    (323,414),
+    (119,437),
+    (136,437),
+    (153,437),
+    (170,437),
+    (187,437),
+    (204,437),
+    (221,437),
+    (238,437),
+    (255,437),
+    (272,437),
+    (289,437),
+    (306,437),
+    (323,437)
+    ]
 
 def loadArtificialNeuralNetwork(punkty):
     # punkty = [(136,230),(272,230),(187,529)]
     img = np.zeros((480,640,1))
 
-    for i in range(len(punkty)-1):
-        start = punkty[i]
-        stop = punkty[i + 1]
-        cv2.line(img, start, stop, (255,255,255), 20, 8)
+    LEARNING_RATE = 1e-4
 
-    newImage = cv2.resize(img, (28, 28))
-
-    obrazekDoSieci=np.array([np.ravel(newImage)])
-
-    LEARNING_RATE = 2.5e-6#jak to zmniejsze i doloze iteracje to uzyskam moj wykres
-    # set to 20000 on local environment to get 0.99 accuracy
-    TRAINING_ITERATIONS = 2000
+    TRAINING_ITERATIONS = 2500
 
     DROPOUT = 0.25
-    BATCH_SIZE = 16#ile probek jest wrzucane do sieci na raz -HIPERARAMETRY
+    BATCH_SIZE = 25#ile probek jest wrzucane do sieci na raz -HIPERARAMETRY
 
     # set to 0 to train on all available data
     VALIDATION_SIZE =2000
@@ -315,7 +306,7 @@ def loadArtificialNeuralNetwork(punkty):
     else:
         saver.restore(sess, 'modele/myModel')
     # check final accuracy on validation set
-    if(VALIDATION_SIZE):
+    if(VALIDATION_SIZE and len(os.listdir('modele'))==0):
         validation_accuracy = accuracy.eval(feed_dict={x: validation_images,
                                                        y_: validation_labels,
                                                        keep_prob: 1.0})
@@ -331,9 +322,11 @@ def loadArtificialNeuralNetwork(punkty):
         plt.show()
 
     # convert from [0:255] => [0.0:1.0]
-    test_images = np.multiply(obrazekDoSieci, 1.0 / 255.0)
+    # test_images = np.multiply(obrazekDoSieci, 1.0 / 255.0)
 
-    # predict test set
+    test_images = pd.read_csv('test.csv').values
+    test_images=test_images.astype(np.float)
+    test_images = np.multiply(test_images, 1.0 / 255.0)
     predicted_lables = predict.eval(feed_dict={x: test_images, keep_prob: 1.0})
     print("wynik:" + str(predicted_lables))
 
@@ -360,3 +353,7 @@ def loadArtificialNeuralNetwork(punkty):
                fmt='%d')
 
     sess.close()
+
+    # displayresult(str(predicted_lables[0]))
+
+loadArtificialNeuralNetwork([])
