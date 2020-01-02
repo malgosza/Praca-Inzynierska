4bf9dab00fbfdccb49120ed42710ca83d232bf61
Gosia <malgosza.niewiadomska@gmail.com>
2020-01-02 19:32:09 +0100

    rozklad bledow

---
+++ wynik.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+def displayresult(result):
+    root=Tk()
+    root.geometry('680x450')
+    root.title("Małgorzata Niewiadomska Inżynieria Biomedyczna")
+
+    Label(text="ROZPOZNANO: \n",
+                font=("arial", 20,'bold')).place(x=250,y=60)
+
+    Label(text=result, fg="green",
+                font=("arial", 120,'bold')
+                ).place(x=300,y=150)
+
+    root.resizable(0, 0)
+    root.mainloop()
+
